afd19a826e83f618658d193db699f041fc193fe6
anecdoche <maltese_harvest@aol.com>
2023-03-15 19:54:04 +0900

    complete Chapter.12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
   get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # Defines the root path route ("/")
   # root "articles#index"
